55cf88868f4cd150a3de7b253b02c8b34fae6438
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-15 14:13:14 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+function triangle(positionX, positionY, size) {
+  context.beginPath();
+  context.moveTo(positionX, positionY);
+  context.lineTo(positionX + size / 2, positionY + size);
+  context.lineTo(positionX - size / 2, positionY + size);
+  context.lineTo(positionX, positionY);
+  context.strokeStyle = 'rgba(0,0,0,.5)';
+  context.fillStyle = 'rgba(255,165,0,.5)';
+  context.stroke();
+  context.fill();
+}
+
+triangle(230, 160, 50);
+triangle(270, 100, 50);
+triangle(200, 50, 150);
